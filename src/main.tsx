ce55f5485f87becf535f76105280cd24f8658a9c
smileywrld <ismaileyyunusa@gmail.com>
2026-02-12 16:21:39 +0100

    refactor: clean up ListGroup component structure and remove unused Fragment import

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.css";
 import App from "./App.tsx";
-import { Fragment } from "react";
 
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
