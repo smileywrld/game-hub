ad0c7420b5b0db3ccc3dcd6965f58ac03f5b0549
smileywrld <ismaileyyunusa@gmail.com>
2026-02-16 04:26:23 +0100

    feat: update ListGroup component to dynamically render items from an array

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,17 +1,18 @@
-
 // import { Fragment } from "react";
 function ListGroup() {
-    
+	const items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+
+	if (items.length === 0) {
+		return <p>No items found</p>
+	}
 	return (
 		<>
-            <ul className="list-group">
-                <li className="list-group-item">An item</li>
-                <li className="list-group-item">A second item</li>
-                <li className="list-group-item">A third item</li>
-                <li className="list-group-item">A fourth item</li>
-                <li className="list-group-item">And a fifth one</li>
-            </ul>
-        </>
+			<ul className="list-group">
+				{items.map((item) => (
+					<li key={item}>{item}</li>
+				))}
+			</ul>
+		</>
 	);
 }
 
