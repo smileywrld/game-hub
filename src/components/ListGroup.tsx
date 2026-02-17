4370a04824862cc23efb3923d4398e9b0c3901fc
smileywrld <ismaileyyunusa@gmail.com>
2026-02-17 06:46:43 +0100

    refactor: clean up ListGroup component by removing unused imports and enhancing item click handling

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,11 +1,16 @@
 // import { Fragment } from "react";
-import { MouseEvent } from "react";
+// import { MouseEvent } from "react";
+
+import { useState } from "react";
 
 function ListGroup() {
 	const items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
 
+	//hook
+	const [selectedIndex, setSelectedIndex] = useState(-1)
+
 	// Event handler
-	const handleClick = (event: MouseEvent) => console.log(event);
+	// const handleClick = (event: MouseEvent) => console.log(event);
 	// const getMessage = () => {
 	// 	return items.length === 0 ? <p>No items found</p> : null;
 
@@ -17,9 +22,17 @@ function ListGroup() {
 			<h1>List</h1>
 			{/* {items.length === 0 ? <p>No items found</p> : null} */}
 			{items.length === 0 && <p>No items found</p>}
-			<ul className="list-group active">
-				{items.map((item) => (
-					<li className="list-group-item" key={item} onClick={handleClick}>
+			<ul className="list-group ">
+				{items.map((item, Index) => (
+					<li
+						className={
+							selectedIndex === Index
+								? "list-group-item active"
+								: "list-group-item"
+						}
+						key={item}
+						onClick={() =>{setSelectedIndex(Index)}}
+					>
 						{item}
 					</li>
 				))}
