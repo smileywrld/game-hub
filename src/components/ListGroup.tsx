4c4647a3e880c93ad57dbfad450477d01610e33a
smileywrld <ismaileyyunusa@gmail.com>
2026-02-18 07:17:31 +0100

    feat: update ListGroup component to accept items and heading as props for dynamic rendering

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,11 +1,17 @@
+
+
 // import { Fragment } from "react";
 // import { MouseEvent } from "react";
 
-import { useState } from "react";
+import { useState } from "react"; 
 
-function ListGroup() {
-	const items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+interface Props{
+	items: string[];
+	heading: string;
 
+}
+
+function ListGroup({items, heading}: Props ) {
 	//hook
 	const [selectedIndex, setSelectedIndex] = useState(-1)
 
@@ -19,7 +25,7 @@ function ListGroup() {
 	// items = [];
 	return (
 		<>
-			<h1>List</h1>
+			<h1>{heading}</h1>
 			{/* {items.length === 0 ? <p>No items found</p> : null} */}
 			{items.length === 0 && <p>No items found</p>}
 			<ul className="list-group ">
