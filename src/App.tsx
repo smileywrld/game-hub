84280376e2f2d4a711bd069c04f944c390f1d0e0
smileywrld <ismaileyyunusa@gmail.com>
2026-02-18 08:01:06 +0100

    feat: add Alert component for displaying messages

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,25 @@
+import Alert from "./components/Alert";
 import ListGroup from "./components/ListGroup";
 
 function App() {
-  const items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
-  const handleSelectItem = (item: string) => {
-    console.log(item)
-  }
+	const items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+	const handleSelectItem = (item: string) => {
+		console.log(item);
+	};
 
 	return (
-		<div>
-      <ListGroup items={ items } heading="Cities" onSelectItems={handleSelectItem}/>
-		</div>
+		<>
+			<div>
+				<ListGroup
+					items={items}
+					heading="Cities"
+					onSelectItems={handleSelectItem}
+				/>
+
+				<Alert>Hello World</Alert>
+			</div>
+		</>
 	);
-}                                                                     
+}
 
 export default App;
